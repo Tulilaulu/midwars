bb0461a3fbf8c64e6b1a48bb62cbfe6c385e9183
Tulilaulu <tulilaulu@gmail.com>
2016-05-17 19:31:12 +0300

    testejä

--- teams/team_name_here/heroes/nymphora_main.lua
+++ teams/team_name_here/heroes/nymphora_main.lua
@@ -46,12 +46,142 @@ local Clamp = core.Clamp
 
 BotEcho('loading nymphora_main...')
 
+local illusionLib = object.illusionLib
+
+local sqrtTwo = math.sqrt(2)
+
 object.heroName = 'Hero_Fairy'
 
 --------------------------------
 -- Lanes
 --------------------------------
 core.tLanePreferences = {Jungle = 0, Mid = 0, ShortSolo = 0, LongSolo = 0, ShortSupport = 5, LongSupport = 5, ShortCarry = 0, LongCarry = 0}
+
+--------------------------------
+-- Items
+--------------------------------
+
+----------------------------------
+--  FindItems Override
+----------------------------------
+local function funcFindItemsOverride(botBrain)
+
+	--Alternate item wasn't checked, so you don't need to look for new items.
+	if core.bCheckForAlternateItems then return end
+
+--	funcRemoveInvalidItems()
+
+	--We only need to know about our current inventory. Stash items are not important.
+	local inventory = core.unitSelf:GetInventory(true)
+	for slot = 1, 6, 1 do
+		local curItem = inventory[slot]
+		if curItem and not curItem:IsRecipe() then
+			if core.itemPostHaste == nil and curItem:GetName() == "Item_PostHaste" then
+				core.itemPostHaste = core.WrapInTable(curItem)
+			elseif core.itemTablet == nil and curItem:GetName() == "Item_PushStaff" then
+				core.itemTablet = core.WrapInTable(curItem)
+			elseif core.itemPortalKey == nil and curItem:GetName() == "Item_PortalKey" then
+				core.itemPortalKey = core.WrapInTable(curItem)
+			elseif core.itemFrostfieldPlate == nil and curItem:GetName() == "Item_FrostfieldPlate" then
+				core.itemFrostfieldPlate = core.WrapInTable(curItem)
+			elseif core.itemSheepstick == nil and curItem:GetName() == "Item_Morph" then
+				core.itemSheepstick = core.WrapInTable(curItem)
+			elseif core.itemHellFlower == nil and curItem:GetName() == "Item_Silence" then
+				core.itemHellFlower = core.WrapInTable(curItem)
+			elseif core.itemSteamboots == nil and curItem:GetName() == "Item_Steamboots" then
+				core.itemSteamboots = core.WrapInTable(curItem)
+			elseif core.itemGhostMarchers == nil and curItem:GetName() == "Item_EnhancedMarchers" then
+				core.itemGhostMarchers = core.WrapInTable(curItem)
+				core.itemGhostMarchers.expireTime = 0
+				core.itemGhostMarchers.duration = 6000
+				core.itemGhostMarchers.msMult = 0.12
+			end
+		end
+		
+	end
+end
+object.FindItemsOld = core.FindItems
+core.FindItems = funcFindItemsOverride
+
+--Check for alternate items before shopping
+local function funcCheckforAlternateItemBuild(botbrain)
+
+	--no further check till next shopping round
+	core.bCheckForAlternateItems = false
+
+	local unitSelf = core.unitSelf
+
+	--initialize item choices
+	if unitSelf.getPK == nil then
+		--BotEcho("Initialize item choices")
+		unitSelf.getSteamboots = false
+		unitSelf.getPushStaff = false
+		unitSelf.getPK = false
+	end
+
+
+	local nGPM = botbrain:GetGPM()
+	local nXPM = unitSelf:GetXPM()
+	local nMatchTime = HoN.GetMatchTime()
+	local bBuyStateLow = behaviorLib.buyState < behaviorLib.BuyStateMidItems
+
+	--Bad early game: skip GhostMarchers and go for more defensive items
+	if bBuyStateLow and nXPM < 170 and nMatchTime > core.MinToMS(5) and not unitSelf.getSteamboots then
+		--BotEcho("My early Game sucked. I will go for a defensive Build.")
+		unitSelf.getSteamboots = true
+		behaviorLib.MidItems =
+		{"Item_Steamboots", "Item_MysticVestments", "Item_Scarab",  "Item_SacrificialStone", "Item_Silence"}
+
+	--Boots finished
+	elseif core.itemGhostMarchers or core.itemSteamboots then
+
+		--Mid game: Bad farm, so go for a tablet
+		if unitSelf:GetLevel() > 10 and nGPM < 240 and not unitSelf.getPushStaff then
+			--BotEcho("Well, it's not going as expected. Let's try a Tablet!")
+			unitSelf.getPushStaff = true
+			tinsert(behaviorLib.curItemList, 1, "Item_PushStaff")
+
+		--Good farm and you finished your Boots. Now it is time to pick a portal key
+		elseif nGPM >= 300 and not unitSelf.getPK then
+			--BotEcho("The Game is going good. Soon I will kill them with a fresh PK!")
+			unitSelf.getPK = true
+			tinsert(behaviorLib.curItemList, 1, "Item_PortalKey")
+		end
+	end
+end
+
+behaviorLib.StartingItems =
+	{"Item_TrinketOfRestoration", "Item_MinorTotem", "Item_ManaPotion", "Item_CrushingClaws"}
+behaviorLib.LaneItems =
+	{"Item_Marchers", "Item_MysticPotpourri", "Item_EnhancedMarchers"}
+behaviorLib.MidItems =
+	{"Item_Astrolabe", "Item_Morph", "Item_FrostfieldPlate"}
+behaviorLib.LateItems =
+	{"Item_Intelligence7", "Item_Summon"} --Intelligence7 is Staff of the Master
+	
+-- SHOPPING OVERRIDE FROM https://github.com/honteam/Heroes-of-Newerth-Bots/blob/Community/bots/heroes/gravekeeper_main.lua
+
+core.bCheckForAlternateItems = true
+local function funcShopExecuteOverride(botBrain)
+	--check item choices
+	if core.bCheckForAlternateItems then
+		--BotEcho("Checking Alternate Builds")
+		funcCheckforAlternateItemBuild(botBrain)
+	end
+
+	local bOldShopping = object.ShopExecuteOld (botBrain)
+
+	--update item links and reset the check
+	if behaviorLib.finishedBuying then
+		core.FindItems()
+		core.bCheckForAlternateItems = true
+		--BotEcho("FindItems")
+	end
+
+	return bOldShopping
+end
+object.ShopExecuteOld = behaviorLib.ShopExecute
+behaviorLib.ShopBehavior["Execute"] = funcShopExecuteOverride
 
 --------------------------------
 -- Skills
